e255da9401f7bf484e3a6573cc0314da208adb8b
Tyrone Meijn <tyrone_meijn@hotmail.com>
2017-03-24 11:20:22 +0100

    Add admin feature
    start building the admin feature of the app.

---
+++ src/app/admin/admin-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    template: `
+            <p>Dashboard</p>
+        `,
+    styles: [``]
+})
+export class AdminDashboardComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
